8962f0561cdf101069585f60950a010771f92ae9
Lakshmi Shaw <Lakshmi.Shaw@Cerner.com>
2020-09-14 11:33:58 +0530

    Adding test plan

---
+++ test/com/company/application/ruleengine/RuleEngineTest.java
@@ -0,0 +1,18 @@
+package com.company.application.ruleengine;
+
+import org.junit.Test;
+
+public class RuleEngineTest {
+	
+	@Test
+	public void bookRuleTest_whenRuleExists() {
+		
+	}
+	
+	@Test()
+	public void bookRuleTest_whenRuleDoesNotExists() {
+	
+		
+	}
+	
+}
---
+++ test/com/company/application/ruleimpl/UpgradeMembershipTest.java
@@ -0,0 +1,21 @@
+package com.company.application.ruleimpl;
+
+import org.junit.jupiter.api.Test;
+
+public class UpgradeMembershipTest {
+
+	@Test
+	public void process_test() {
+		
+	}
+
+	@Test
+	public void matches_test_when_ruleExists() {
+	
+	}
+
+	@Test
+	public void matches_test_when_ruleDoesNotExists() {
+		
+	}
+}
